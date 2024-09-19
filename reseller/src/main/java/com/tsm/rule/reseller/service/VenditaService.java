72debcfbc1df0f0cbbab7779d0b96677d63cb9e5
Simone Piccirilli <simonepiccirilli@MBP-di-Simone.lan>
2024-09-20 00:52:23 +0200

    vendita implemented with subtask

--- reseller/src/main/java/com/tsm/rule/reseller/service/VenditaService.java
+++ reseller/src/main/java/com/tsm/rule/reseller/service/VenditaService.java
@@ -3,6 +3,7 @@ package com.tsm.rule.reseller.service;
 import com.tsm.rule.reseller.exception.ResellerException;
 import com.tsm.rule.reseller.io.request.VenditaRequest;
 import com.tsm.rule.reseller.io.response.BaseResponse;
+import com.tsm.rule.reseller.model.base.AcquistoBase;
 import com.tsm.rule.reseller.model.entity.CartePokemon;
 import com.tsm.rule.reseller.model.entity.OggettoGenerico;
 import com.tsm.rule.reseller.model.entity.Vendita;
@@ -40,14 +41,14 @@ public class VenditaService {
         if(OggettiTrattati.POKEMON.equals(request.tipoOggetto()))
             acqusito = cartaPokemonRepo.findByChiaveOggetto(request.chiaveAcquisto())
                     .orElseThrow(() -> {
-                        log.error("Error on salvaVendita service, missing entity on db: {}, with this chiave: {}",request.tipoOggetto(),request.chiaveAcquisto());
-                        return new ResellerException("Error on salvaVendita service",null,"Missing entity");
+                        log.error("Error on salvaVendita service , missing entity on db: {}, with this chiave: {}",request.tipoOggetto(),request.chiaveAcquisto());
+                        return new ResellerException("Error on salvaVendita service pkm",null,"Missing entity pkm");
                     });
         else
             acqusito =  oggettoGenericoRepo.findByChiaveOggetto(request.chiaveAcquisto())
                     .orElseThrow(() -> {
-                        log.error("Error on salvaVendita service, missing entity on db: {}, with this chiave: {}",request.tipoOggetto(),request.chiaveAcquisto());
-                        return new ResellerException("Error on salvaVendita service",null,"Missing entity");
+                        log.error("Error on salvaVendita service , missing entity on db: {}, with this chiave: {}",request.tipoOggetto(),request.chiaveAcquisto());
+                        return new ResellerException("Error on salvaVendita service generico",null,"Missing entity generico");
                     });
         var entity = new Vendita();
         var nome = "";
@@ -118,7 +119,7 @@ public class VenditaService {
                 var task1 = scope.fork(() -> cartaPokemonRepo.findByChiaveOggetto(request.chiaveAcquisto()));
                 var acq = task1.get().orElseThrow(() -> {
                     log.error("Error on salvaVendita service, missing entity on db: {}, with this chiave: {}",request.tipoOggetto(),request.chiaveAcquisto());
-                    return new ResellerException("Error on salvaVendita service",null,"Missing entity");
+                    return new ResellerException("Error on salvaVendita service pkm",null,"Missing entity");
                 });
                 if(ObjectUtils.isEmpty(entity.getNome()))
                     entity.setNome(acq.getNome());
@@ -127,7 +128,7 @@ public class VenditaService {
                 var task2 = scope.fork(() -> oggettoGenericoRepo.findByChiaveOggetto(request.chiaveAcquisto()));
                 var acq = task2.get().orElseThrow(() -> {
                     log.error("Error on salvaVendita service, missing entity on db: {}, with this chiave: {}",request.tipoOggetto(),request.chiaveAcquisto());
-                    return new ResellerException("Error on salvaVendita service",null,"Missing entity");
+                    return new ResellerException("Error on salvaVendita service generico",null,"Missing entity");
                 });
 
                 if(ObjectUtils.isEmpty(entity.getNome()))
@@ -152,7 +153,76 @@ public class VenditaService {
     }
 
     // una vendita non si puo modificare, ma solo cancellare e ricreare
+    @Transactional
     public BaseResponse deleteVendita(Integer idVendita){
         log.info("DeleteVendita service started for idVendita: {}",idVendita);
+        // retrivo vendita
+        var vendita = venditaRepo.findById(idVendita).orElseThrow(() -> {
+            log.error("Error on DeleteVendita service, missing entity on db: ");
+            return new ResellerException("Error on DeleteVendita service find",null,"Missing vendita");
+        });
+        Object acquisto = null;
+        if(OggettiTrattati.POKEMON.toString().equals(vendita.getTipoOggetto()))
+            acquisto = cartaPokemonRepo.findByChiaveOggetto(vendita.getChiaveAcquisto())
+                    .orElseThrow(() -> {
+                        log.error("Error on deleteVendita service, missing entity acquisto pokemon");
+                        return new ResellerException("Error on deleteVendita service",null,"Missing acqusto pokemon");
+                    });
+        else
+            acquisto =  oggettoGenericoRepo.findByChiaveOggetto(vendita.getChiaveAcquisto())
+                    .orElseThrow(() -> {
+                        log.error("Error on deleteVendita service, missing entity acquisto generico");
+                        return new ResellerException("Error on deleteVendita service",null,"Missing acqusto generico");
+                    });
+        // risetto quantia originale
+        var quantitaRollback = ((AcquistoBase) acquisto).getQuantitaDisponibile() + vendita.getQuantita();
+        // setto
+        ((AcquistoBase) acquisto).setQuantitaDisponibile(quantitaRollback);
+        try(var scope = new StructuredTaskScope.ShutdownOnFailure()){
+        if(OggettiTrattati.POKEMON.toString().equals(vendita.getTipoOggetto())) {
+            var finalAcquisto = (CartePokemon) acquisto;
+            scope.fork(() ->cartaPokemonRepo.save(finalAcquisto));
+        }
+        else {
+            var finalAcquisto = (OggettoGenerico) acquisto;
+            scope.fork(() -> oggettoGenericoRepo.save(finalAcquisto));
+        }
+        // chiamo metodo che torna oggetto se no si incazza malamente, scoprire perche, ma contando che euna previuw
+        var finalTask = scope.fork(() -> deleteVendita(vendita));
+        // joino e twowo se faila
+        scope.join().throwIfFailed();
+        // prendo resp, loggo e torno
+        var resp = finalTask.get();
+        log.info("DeleteVendita service ended successfully");
+        return resp;
+        }catch (InterruptedException e){
+            log.error("Error on DeleteVendita service, with err: {}",e.getMessage());
+            throw new ResellerException("Error on DeleteVendita service",e,e.getMessage());
+        }
+        catch (ExecutionException e){
+            log.error("Error on DeleteVendita service with error: {}",e.getMessage());
+            throw new ResellerException("Error on DeleteVendita service",e,e.getMessage());
+        }
     }
+
+    public Vendita getVenditaById(Integer id){
+        log.info("GetVenditaById service started");
+
+        var vendita = venditaRepo.findById(id).orElseThrow(() -> {
+            log.error("Error on getVenditaById service, missing entity on db: ");
+            return new ResellerException("Error on getVenditaById service",null,"Missing vendita");
+        });
+
+        log.info("GetVenditaById ended Successfully");
+        return vendita;
+    }
+
+    // ho dovuto mettere sta cosa qua perche deve tornare per foza un cazzo di oggetto se uso scope
+    private BaseResponse deleteVendita(Vendita vendita){
+        venditaRepo.delete(vendita);
+        return new BaseResponse("Deleted successfully","00");
+    }
+
+
+    //TODO: fare test dei junit di sta roba se funziona tutta la parte vendita
 }
