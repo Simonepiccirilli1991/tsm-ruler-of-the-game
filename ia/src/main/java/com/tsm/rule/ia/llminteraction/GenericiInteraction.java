adfda0096764caab8e0e656f8fe80e0c5a0cd385
Simone Piccirilli <simonepiccirilli@MBP-di-Simone.lan>
2024-10-15 22:15:31 +0200

    started ia handler

---
+++ ia/src/main/java/com/tsm/rule/ia/llminteraction/GenericiInteraction.java
@@ -0,0 +1,13 @@
+package com.tsm.rule.ia.llminteraction;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class GenericiInteraction {
+
+    //TODO: inserire interazione con ollama , integrare le news di m3 di spring ai, e java 23
+}
